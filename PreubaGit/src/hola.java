adeacc843c82bda6a86ee1ac69348e089b95de69
OscarOrtizHdez <oscar.ortiz@eisei.net.mx>
2016-12-28 13:12:15 -0600

    si eso es

--- PreubaGit/src/hola.java
+++ PreubaGit/src/hola.java
@@ -3,7 +3,8 @@ public class hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sdfsfsdafasd");
+		System.out.println("Si si  si a huevo");
+		
 	}
 
 }
